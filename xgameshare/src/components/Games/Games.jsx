12f5c56dd7fc85335d27faf9dd339bdd5d881309
Ksaadoon <Ksaadoon@gmail.com>
2023-06-06 20:04:41 -0400

    Games component

--- xgameshare/src/components/Games/Games.jsx
+++ xgameshare/src/components/Games/Games.jsx
@@ -1,20 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import * as igdbService from '../../services/games/igdb/igdb-service';
 
-const Games = ({twitchAccessToken}) => {
+const Games = () => {
 
   const [games, setGames] = useState([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
-    const res = igdbService.listGames();
-  });
+    const fetchGames = async () => {
+      try {
+        const res = await igdbService.listGames();
+        setGames(res); // Update the games state with the response data
+      } catch (error) {
+        setError('Failed to fetch games'); // Update the error state with an error message
+      }
+    };
 
-  
+    fetchGames();
+  }, []);
+
+
 
   return (
-    <div>Games</div>
-  )
-  }
+    <div>
+      {games.length > 0 ? (
+        // Render the list of games
+        <ul>
+          {games.map((game) => (
+            <li key={game.id}>{game.id}</li>
+          ))}
+        </ul>
+      ) : error ? (
+        // Render the error message
+        <div>{error}</div>
+      ) : (
+        // Render a loading indicator or placeholder
+        <div>Loading games...</div>
+      )}
+    </div>
+  );
+}
 
-export default Games
+export default Games;
